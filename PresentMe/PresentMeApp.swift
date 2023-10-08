ccd84c13b9ba144b70c6820709e3f3c34d09de4f
lujainalaydie1 <lujainalaydie1@gmail.com>
2023-10-08 16:50:47 +0300

    Initial Commit

---
+++ PresentMe/PresentMeApp.swift
@@ -0,0 +1,17 @@
+//
+//  PresentMeApp.swift
+//  PresentMe
+//
+//  Created by Lujain Alaydie on 08/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct PresentMeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
